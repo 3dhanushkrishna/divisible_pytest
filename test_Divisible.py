515247cb05bca7beac7e0850b0610a4c4889d538
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-03-28 12:34:34 +0530

    fixture

--- test_Divisible.py
+++ test_Divisible.py
@@ -1,18 +1,22 @@
 import Divisible
 import pytest
-def test_divisible5():
-    x=5
-    result=Divisible.divisible_5(x)
+@pytest.fixture
+def input():
+    x=25
+    return x
+def test_divisible5(input):
+
+    result=Divisible.divisible_5(input)
     assert result==True
-def test_divisible7():
-    x=14
-    result=Divisible.divisible_7(x)
-    assert result==True
-def test_divisible9():
-    x=27
-    result=Divisible.divisible_9(x)
-    assert result==True
-def test_divisible10():
-    x=50
-    result=Divisible.divisible_10(x)
-    assert result==True
+def test_divisible7(input):
+
+    result=Divisible.divisible_7(input)
+    assert result==False
+def test_divisible9(input):
+
+    result=Divisible.divisible_9(input)
+    assert result==False
+def test_divisible10(input):
+
+    result=Divisible.divisible_10(input)
+    assert result==False
